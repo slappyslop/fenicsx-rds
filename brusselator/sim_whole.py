1930f498f42d2a826f4e9c24dd202838d160897d
Shravan Kumar <kshravan694@gmail.com>
2025-07-15 20:50:55 +0000

    Added a very simple evolution law

--- brusselator/sim_whole.py
+++ brusselator/sim_whole.py
@@ -3,7 +3,7 @@ import numpy as np
 from dolfinx import mesh, fem
 import ufl
 from mpi4py import MPI
-from dolfinx.io import VTKFile, XDMFFile
+from dolfinx.io import VTXWriter, XDMFFile
 from basix.ufl import mixed_element
 import dolfinx.fem.petsc
 import dolfinx.nls.petsc
@@ -14,11 +14,13 @@ def on_equator(x):
     return np.isclose(x[2], 0.0, atol=1e-3)  
 
 def initialize_function(functionSpace, initial):
+    '''Creates a function on a functionspace with initial conditions'''
     function = fem.Function(functionSpace)
     function.x.array[:] = initial
     return function
 
 def initialize_peturbation(functionSpace, initial):
+    '''Creates a function with a small random peturbation around initial'''
     function = fem.Function(functionSpace)
     if initial == 0:
         function.x.array[:] = 0.01 * (np.random.rand(len(function.x.array)) - 0.5)
@@ -26,85 +28,73 @@ def initialize_peturbation(functionSpace, initial):
         function.x.array[:] = initial + 0.01 * initial * (np.random.rand(len(function.x.array[:])) - 0.5)
     return function
 
+def compute_norm(new, old):
+    diff = new - old
+    return np.sqrt(fem.assemble_scalar(fem.form(diff * diff * ufl.dx))) / params.dt
+
+def normalize(values):
+    values = np.array(values)
+    return (values - values.min()) / (values.max() - values.min() + 1e-10)
+
+def compute_normals(mesh: dolfinx.mesh.Mesh):
+    cells = mesh.topology.connectivity(2, 0).array
+    coords = domain.geometry.x
+    element_normals = np.zeros((cells.shape[0] // 3,3))
+    for i in range(0, len(cells), 3):
+        v0 = coords[cells[i]]
+        v1 = coords[cells[i+1]]
+        v2 = coords[cells[i+2]]
+        n = np.cross(v1 - v0, v2-v0)
+        n /= np.linalg.norm(n) + 1e-14
+        element_normals[i//3] = n
+
+    vertex_normals = np.zeros_like(coords)
+    counts = np.zeros((coords.shape[0], 1))
+    for i in range(0,len(cells),3):
+        for j in range(3):
+            v = cells[i + j]
+            vertex_normals[v] += element_normals[i//3]
+            counts[v] += 1
+    vertex_normals /= counts
+    norms = np.linalg.norm(vertex_normals, axis=1, keepdims=True)
+    vertex_normals /= norms + 1e-14
+    return vertex_normals
+    
+
 
 with XDMFFile(MPI.COMM_WORLD, "out_gmsh/hemisphere.xdmf", "r") as xdmf:
     domain = xdmf.read_mesh(name="hemisphere")
 
-V = fem.functionspace(domain, ("CG", 1))
+V = fem.functionspace(domain, ("CG", 1,))
 W = fem.functionspace(domain, mixed_element([V.ufl_element(), V.ufl_element(), V.ufl_element(), V.ufl_element()]))
+D = fem.functionspace(domain, ("CG", 1, (domain.geometry.dim,)))
 X0 = params.x1_star
 Y0 = params.y1_star
 X2_0 = 0 # change maybe
 Y2_0 = 0  # change maybe
-
+intial_values = [X0, Y0, X2_0, Y2_0]
 
 
 fdim = domain.topology.dim - 1  # 1 if surface mesh (dim=2)
 domain.topology.create_connectivity(fdim, domain.topology.dim)
 equator_facets = mesh.locate_entities_boundary(domain, fdim, on_equator)
-# bc_value_u = fem.Function(V)
-# bc_value_u.x.array[:] = X0
 
-# bc_value_v = fem.Function(V)
-# bc_value_v.x.array[:] = Y0
-
-# bc_value_u2 = fem.Function(V)
-# bc_value_u2.x.array[:] = X2_0
-
-# bc_value_v2 = fem.Function(V)
-# bc_value_v2.x.array[:] = Y2_0
-
-bc_value_u = initialize_function(V, X0)
-bc_value_v= initialize_function(V, Y0)
-bc_value_u2 = initialize_function(V, X2_0)
-bc_value_v2 = initialize_function(V, Y2_0)
-
-
-dofs_u = fem.locate_dofs_topological((W.sub(0), V), fdim, equator_facets)
-dofs_v = fem.locate_dofs_topological((W.sub(1), V), fdim, equator_facets)
-dofs_u2 = fem.locate_dofs_topological((W.sub(2), V), fdim, equator_facets)
-dofs_v2 = fem.locate_dofs_topological((W.sub(3), V), fdim, equator_facets)
-
-# Define DirichletBC objects
-bc_u = fem.dirichletbc(bc_value_u, dofs_u, W.sub(0))
-bc_v = fem.dirichletbc(bc_value_v, dofs_v, W.sub(1))
-bc_u2 = fem.dirichletbc(bc_value_u2, dofs_u2, W.sub(2))
-bc_v2 = fem.dirichletbc(bc_value_v2, dofs_v2, W.sub(3))
-
-
-## Random peturbation
-# u0 = fem.Function(V)
-# v0 = fem.Function(V)
-# u2_0 = fem.Function(V)
-# v2_0 = fem.Function(V)
-# u0.x.array[:] = X0 + 0.01 * X0 * (np.random.rand(len(u0.x.array)) - 0.5)
-# v0.x.array[:] = Y0 + 0.01 * Y0 * (np.random.rand(len(v0.x.array)) - 0.5)
-# if (X2_0 == 0):
-#     u2_0.x.array[:] = 0.01 *(np.random.rand(len(u2_0.x.array)) - 0.5)
-# else:
-#     u2_0.x.array[:] = X2_0 + 0.01 * X2_0 *(np.random.rand(len(u2_0.x.array)) - 0.5)
-
-# if (Y2_0 == 0):
-#     v2_0.x.array[:] =  0.01 * (np.random.rand(len(v2_0.x.array)) - 0.5)
-# else:
-#     v2_0.x.array[:] = Y2_0 + 0.01 * Y2_0 * (np.random.rand(len(v2_0.x.array)) - 0.5)
-
-u0 = initialize_peturbation(V, X0)
-v0 = initialize_peturbation(V, Y0)
-u2_0 = initialize_peturbation(V, X2_0)
-v2_0 = initialize_peturbation(V, Y2_0)
-
-
-
-
+## Set BCs and ICs
+bcs = []
+ics = []
+for i, iv in enumerate(intial_values):
+    sub = W.sub(i) # select i
+    bc_value = initialize_function(V, iv)
+    dofs = fem.locate_dofs_topological((sub, V), fdim, equator_facets)
+    bcs.append(fem.dirichletbc(bc_value, dofs, sub))
+    ## set intial condition
+    ics.append(initialize_peturbation(V, iv))
+ 
 # Combine into a mixed function
 w0 = fem.Function(W)
-w0.sub(0).interpolate(u0)
-w0.sub(1).interpolate(v0)
-w0.sub(2).interpolate(u2_0)
-w0.sub(3).interpolate(v2_0)
-w = fem.Function(W)
-w.x.array[:] = w0.x.array
+for sub, ic in zip(w0.split(), ics):
+    sub.interpolate(ic)
+
 w  = initialize_function(W, w0.x.array)
 
 # Trial, test functions
@@ -123,51 +113,48 @@ f4 = (1/params.dt) * ufl.inner(v2 - v2_n, phi_v2) * ufl.dx \
      - params.g_2(u_n, v_n, u2_n, v2_n) * phi_v2 * ufl.dx
 F = f1 + f2 + f3 + f4
 
-problem = dolfinx.fem.petsc.NonlinearProblem(F, w, bcs = [bc_u, bc_v, bc_u2, bc_v2])
+problem = dolfinx.fem.petsc.NonlinearProblem(F, w, bcs = bcs)
 solver = dolfinx.nls.petsc.NewtonSolver(domain.comm, problem)
 solver.rtol = 1e-6
- 
-vtk_u = VTKFile(domain.comm, "rewrite_hemisphere_b/bruss_u_implicit.pvd", "w")
-vtk_v = VTKFile(domain.comm, "rewrite_hemisphere_b/bruss_v_implicit.pvd", "w")
-vtk_u2 = VTKFile(domain.comm, "rewrite_hemisphere_b/gm_u2_implicit.pvd", "w")
-vtk_v2 = VTKFile(domain.comm, "rewrite_hemisphere_b/gm_v2_implicit.pvd", "w")
 
 u_out = fem.Function(V)
 v_out = fem.Function(V)
 u2_out = fem.Function(V)
 v2_out = fem.Function(V)
 
+writer_u = VTXWriter(domain.comm, "hemisphere_vtx/out_u.bp", [u_out])
+writer_v = VTXWriter(domain.comm, "hemisphere_vtx/out_v.bp", [v_out])
+writer_u2 = VTXWriter(domain.comm, "hemisphere_vtx/out_u2.bp", [u2_out])
+writer_v2 = VTXWriter(domain.comm, "hemisphere_vtx/out_v2.bp", [v2_out])
+
 t = 0
 step = 0
 L2normU, L2normV, L2normU2, L2normV2 = [], [], [], []
+dofs = fem.locate_dofs_topological(D, fdim, equator_facets)
+u_d = fem.Function(D)
+u_d.interpolate(lambda x: np.stack((0.001*np.ones_like(x[0]), np.zeros_like(x[0]), np.zeros_like(x[0]))))
+u_d.x.array[dofs] = 0.0
+deformation_array = u_d.x.array.reshape((-1, D.mesh.geometry.dim))  
+
 
 while t < params.T:
     t += params.dt
     step += 1
     
-
     n, converged = solver.solve(w)
     if not converged:
-        print(f"Step {step}: Newton solver failed to converge")
+        print(f"Step {step}: Newton solver,   failed to converge")
         break
-
 
     # Split updated solution
     u_sol, v_sol, u2_sol, v2_sol = w.split()
-    u_out.interpolate(u_sol)
-    v_out.interpolate(v_sol)
-    u2_out.interpolate(u2_sol)
-    v2_out.interpolate(v2_sol)
-
+    for out, sol in zip([u_out, v_out, u2_out, v2_out], w.split()):
+        out.interpolate(sol)
+    
 
     # Split previous solution for norms
     u_old, v_old, u2_old, v2_old = w0.split()
-
-    # Compute L2 norms
-    def compute_norm(new, old):
-        diff = new - old
-        return np.sqrt(fem.assemble_scalar(fem.form(diff * diff * ufl.dx))) / params.dt
-
+    
     norm_u = compute_norm(u_sol, u_old)
     norm_v = compute_norm(v_sol, v_old)
     norm_u2 = compute_norm(u2_sol, u2_old)
@@ -177,21 +164,23 @@ while t < params.T:
     L2normV.append(norm_v)
     L2normU2.append(norm_u2)
     L2normV2.append(norm_v2)
+    for l2n, n in zip([L2normU, L2normV, L2normU2, L2normV2], [norm_u, norm_v, norm_u2, norm_v2]):
+        l2n.append(n)
 
-   
     if step % 50 == 0:
         print(f"Step {step}, t = {t:.3f}, Newton iterations = {n}")
-        vtk_u.write_function(u_out, t)
-        vtk_v.write_function(v_out, t)
-        vtk_u2.write_function(u2_out, t)
-        vtk_v2.write_function(v2_out, t)
+        if t > 100:
+            vertex_normals = compute_normals(domain)
+            growth_values = u2_out.x.array
+            deformation = params.c_g * growth_values[:, None] * vertex_normals
+            domain.geometry.x[:]   += deformation
+        for writer in [writer_u, writer_v, writer_u2, writer_v2]:
+            writer.write(t)
+        
 
     # Update previous solution for next step
     w0.x.array[:] = w.x.array
 
-def normalize(values):
-    values = np.array(values)
-    return (values - values.min()) / (values.max() - values.min() + 1e-10)
 
 L2normU_normalized = normalize(L2normU)
 L2normV_normalized = normalize(L2normV)
